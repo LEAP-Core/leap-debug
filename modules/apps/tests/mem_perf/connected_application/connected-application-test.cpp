c32931f642762db8562818b2dd3b5a2256423d76
Elliott Fleming <kermin.fleming@intel.com>
2013-03-05 22:20:43 +0000

    CSN: CSN-leap-debug-51
    Removed second scratchpad from mem-perf.

--- modules/apps/tests/mem_perf/connected_application/connected-application-test.cpp
+++ modules/apps/tests/mem_perf/connected_application/connected-application-test.cpp
@@ -40,12 +40,12 @@ CONNECTED_APPLICATION_CLASS::Main()
     //
 
     for (int rw = 0; rw < 2; rw++) {
-        for (int ws = 9; ws < 10; ws++) {
+        for (int ws = 9; ws < 23; ws++) {
             for (int stride_idx = 0; stride_idx < 2; stride_idx++) {
                 stringstream filename;
                 OUT_TYPE_RunTest result = clientStub->RunTest(1 << ws,
                                                               stride[stride_idx],
-                                                              1<<12,
+                                                              1<<20,
                                                               rw);
 
                 filename << "cache_" << rw << "_" << stride_idx << "_" << ws << ".stats";
